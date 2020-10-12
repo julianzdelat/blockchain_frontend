a527c6bdbe303b26ba707eb0c182e19839e829a0
Andres Chavez <andres.chavez@toshibagcs.com>
2020-10-11 22:53:52 -0500

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV,
 
-  entry: "./src/index.jsx",
+  entry: './src/index.jsx',
 
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].[hash].js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[hash].js',
+    publicPath: '/',
   },
 
   devtool: process.env.NODE_ENV === 'production' ? undefined : 'inline-source-map',
 
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
 
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
-      }
+      },
     ],
   },
 
@@ -33,9 +33,9 @@ module.exports = {
     new CleanWebpackPlugin(),
 
     new HtmlWebpackPlugin({
-      template: __dirname + "/src/index.html",
-      filename: "index.html",
-      inject: "body",
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
     }),
   ],
 };
