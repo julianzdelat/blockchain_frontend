755c695fb167994391bda72ed1bb3475f71e480b
andreschabb <andres.chavez@wizeline.com>
2021-02-27 15:15:10 -0600

    Issue #4

--- src/pages/Items/Items.page.jsx
+++ src/pages/Items/Items.page.jsx
@@ -1,10 +1,17 @@
 import './Items.styles.css';
 import React from 'react';
 
+import mockedData from './items-mock.json';
+import ItemsTable from '../../components/ItemsTable';
+import { ITEM_HEADERS } from '../../utils/constants';
+
+const { items } = mockedData;
+
 const ItemsPage = () => {
   return (
     <section className="itemspage">
-      <h1>ItemsPage</h1>
+      <h1>Items</h1>
+      <ItemsTable items={items} headers={ITEM_HEADERS} />
     </section>
   );
 };
